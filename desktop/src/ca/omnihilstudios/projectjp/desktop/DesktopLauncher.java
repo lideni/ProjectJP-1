140849c4d53402375af4c2df5e66fc168a79ee40
serity93 <serity93@gmail.com>
2016-01-23 00:26:23 -0800

    Added files from LibGDX project generator.

---
+++ desktop/src/ca/omnihilstudios/projectjp/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ca.omnihilstudios.projectjp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.omnihilstudios.projectjp.ProjectJP;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectJP(), config);
+	}
+}
